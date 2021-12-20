31ee9e3c66cd56df1e21f3b168c609cca35286de
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-20 18:25:56 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -29,11 +29,11 @@ async def ForceSub(bot: Client, cmd: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            text="**Please Join My Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("🤖 Channel", url=invite_link.invite_link)
                     ],
                     [
                         InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshFsub")
@@ -46,7 +46,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/hb4all1_bot).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
