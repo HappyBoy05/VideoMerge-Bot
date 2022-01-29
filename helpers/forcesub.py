9e97982199da648b67cf0e6d81b3abf22205ce29
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2022-01-29 13:23:36 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -46,7 +46,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/hb4all1_bot).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/hb4all_support).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
